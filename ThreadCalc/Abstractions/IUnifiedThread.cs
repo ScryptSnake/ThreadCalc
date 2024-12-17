871bc35a5f5bad245216af925c4ded59ed2d399f
BM <bmiller.pct@gmail.com>
2024-12-16 22:09:20 -0500

    Completed base class (record) for Thread. All threads will inherit from this object. Contains common properties for all types of threads.
    Started building interfaces for specific thread standards.

---
+++ ThreadCalc/Abstractions/IUnifiedThread.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadCalc.Abstractions
+{
+
+    /// <summary>
+    /// Defines properties for which a UTS thread must contain.
+    /// </summary>
+    internal interface IUnifiedThread
+    {
+
+
+
+
+
+
+    }
+}
---
+++ ThreadCalc/Abstractions/Thread.cs
@@ -0,0 +1,20 @@
+﻿using ThreadCalc.Types;
+
+namespace ThreadCalc.Core;
+
+
+/// <summary>
+/// A base class (record) from which all threads inherit.
+/// </summary>
+public record Thread(
+    
+    ThreadStandards Standard, 
+    decimal BasicSize,
+    decimal Pitch,
+    ThreadOrientations Orientation,
+    bool IsCustom
+
+    )
+{
+
+}
